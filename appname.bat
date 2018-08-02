fcb883fc79e49df32f6711747506a817fcb28d53
Bill Chatfield <bill.chatfield@cardinalhealth.com>
2018-08-02 12:07:40 -0400

    Added better error handling and Java install option

--- appname.bat
+++ appname.bat
@@ -1,17 +1,37 @@
 @echo off
-rem //////////////////////////////////////////////////////////////////////
-rem
-rem Java App Run Script
-rem Author: Bill Chatfield
-rem License: Apache 2.0
-rem
-rem //////////////////////////////////////////////////////////////////////
-rem Skip over subroutine definitions.
-goto :main
 
 rem //////////////////////////////////////////////////////////////////////
 rem
-rem Writes a message if DEBUG is turned on.
+rem Java Run Script
+rem
+rem Author:         Bill Chatfield
+rem
+rem License:        Apache License 2.0
+rem
+rem Instructions:   Name this script the same as the jar file you want
+rem                 to run and then run it.
+rem
+rem //////////////////////////////////////////////////////////////////////
+
+
+rem Skip over subroutine definitions.
+goto :main
+
+
+rem //////////////////////////////////////////////////////////////////////
+rem
+rem Subroutine to write a message to the standard output stream.
+rem
+rem //////////////////////////////////////////////////////////////////////
+:writeMessage
+echo %programName%: %*
+exit /b
+rem The above "exit" returns from this subroutine.
+
+
+rem //////////////////////////////////////////////////////////////////////
+rem
+rem Subroutine to write a debug message if DEBUG is turned on.
 rem
 rem //////////////////////////////////////////////////////////////////////
 :writeDebug
@@ -19,9 +39,11 @@ if defined DEBUG echo %programName%: DEBUG: %*
 exit /b
 rem The above "exit" returns from this subroutine.
 
+
 rem //////////////////////////////////////////////////////////////////////
 rem
-rem Writes an error message to the standard error output stream.
+rem Subroutine to write an error message to the standard error output
+rem stream.
 rem
 rem //////////////////////////////////////////////////////////////////////
 :writeError
@@ -37,31 +59,27 @@ rem
 rem //////////////////////////////////////////////////////////////////////
 :main
 setlocal EnableDelayedExpansion
-set programName=%~0
+set programName=%~nx0
 title %programName%
 rem Determine the directory this script is in and then remove the trailing
 rem backslash.
 set binDir=%~dp0
 set binDir=%binDir:~0,-1%
-set jarName=%programName%.jar
+set jarName=%~n0.jar
+set jarSearchDirs="%binDir%" "%binDir%\..\lib" "%USERPROFILE%\lib" "%USERPROFILE%\Documents\lib" "%USERPROFILE%\Dropbox\lib"
+set javaSearchDirs="C:\j2sdk1.4*" "C:\j2sdk1.5*" "C:\Program Files (x86)\Java\jre*" "C:\Program Files (x86)\Java\jdk*" "C:\Program Files\Java\jre*" "C:\Program Files\Java\jdk*"
 
 rem //////////////////////////////////////////////////////////////////////
 rem
 rem Find application jar file to run
 rem
 rem //////////////////////////////////////////////////////////////////////
-
-rem DO NOT try to put the search directories in a variable first because
-rem then any space inside an individual directory will cause the directory
-rem to get split into separate words. Putting the list in the "for" loop
-rem allows the spaces in directory names to be preserved with the double-
-rem quotes around each variable.
 set jar=
-for /d %%d in ("%binDir%" "%binDir%\..\lib" "%USERPROFILE%\lib" "%USERPROFILE%\Documents\lib" "%USERPROFILE%\Dropbox\lib") do (
-    set potentialJarLocation=%%~d\%jarName%
-    call :writeDebug Checking potentialJarLocation=!potentialJarLocation!
-    if exist "!potentialJarLocation!" (
-        set jar=!potentialJarLocation!
+for /d %%d in (%jarSearchDirs%) do (
+    set checkJar=%%~d\%jarName%
+    call :writeDebug Checking checkJar=!checkJar!
+    if exist "!checkJar!" (
+        set jar=!checkJar!
         call :writeDebug Found jar=!jar!
         rem No need to waste CPU time continuing this loop.
         goto :endOfJarSearch
@@ -70,7 +88,12 @@ for /d %%d in ("%binDir%" "%binDir%\..\lib" "%USERPROFILE%\lib" "%USERPROFILE%\D
 :endOfJarSearch
 
 if not defined jar (
-    call :writeError Application jar %jarName% not found.
+    call :writeError Failed to find %jarName% in any of these locations:
+    for %%j in (%jarSearchDirs%) do (
+        call :writeError + %%~j
+    )
+    call :writeError Please move %jarName% into one of the above locations
+    call :writeError and try again.
     goto :end
 )
 
@@ -85,38 +108,56 @@ set java=
 
 rem Check for java.exe in JAVA_HOME\bin
 if defined JAVA_HOME (
-    set possibleJava=!JAVA_HOME!\bin\java.exe
-    if exist "!possibleJava!" (
-        set java=!possibleJava!
+    set checkJava=!JAVA_HOME!\bin\java.exe
+    if exist "!checkJava!" (
+        set java=!checkJava!
     )
 )
 
+rem 
+rem Check for java in the PATH. This should be the case for a modern
+rem version of java, unless the user has modified their PATH variable.
+rem
 if not defined java (
-    rem Check for java in the PATH. This should be the case for a modern
-    rem version of java, unless the user has modified their PATH variable.
     where java
     if %ERRORLEVEL%==0 (
         set java=java
     )
 )
 
+rem
+rem Find the highest version of Java. The last one set will be the
+rem highest version because they're processed in alphabetic order.
+rem
 if not defined java (
-    rem Find the highest version of Java. The last one set will be the
-    rem highest version because they're processed in alphabetic order.
-    for /d %%j in ("C:\Program Files (x86)\Java\jre*" ^
-                   "C:\Program Files (x86)\Java\jdk*" ^
-                   "C:\Program Files\Java\jre*" ^
-                   "C:\Program Files\Java\jdk*") ^
-                   do (
-        set possibleJava=%%j\bin\java.exe
-        if exist "!possibleJava!" (
-            set java=!possibleJava!
+    for /d %%j in () do (
+        set checkJava=%%j\bin\java.exe
+        call :writeDebug checkJava=!checkJava!
+        if exist "!checkJava!" (
+            set java=!checkJava!
         )
     )
 )
 
+rem 
+rem Handle missing Java.
+rem 
 if not defined java (
-    call :writeError Java not found. Please install it or set JAVA_HOME.
+    call :writeError Failed to find Java in any standard install directory.
+    call :writeError Please install Java or set JAVA_HOME to the directory
+    call :writeError where you have installed Java.
+    choice /c YN /m "Do you want to download and install Java now"
+    if !ERRORLEVEL!==1 (
+        rem User answered "Yes".
+        call :writeMessage Opening Java download web site in web browser...
+        start https://www.java.com/download
+        call :writeMessage Java Installation Instructions:
+        call :writeMessage 1. Click the download button in your browser window.
+        call :writeMessage 2. Wait for the Java install file to download.
+        call :writeMessage 3. Double-click the Java install file that was downloaded.
+        call :writeMessage 4. Follow the prompts to install Java.
+        call :writeMessage 5. Run %programName% again after Java is installed.
+    )
     goto :end
 )
 
@@ -126,9 +167,14 @@ rem All parts found. Run the app.
 rem
 rem //////////////////////////////////////////////////////////////////////
 
+title %jar%
+
 "%java%" -jar "%jar%" %*
 
+title %programName%
+
 :end
+
 
 rem //////////////////////////////////////////////////////////////////////
 rem
@@ -140,6 +186,11 @@ call :writeDebug CMDCMDLINE=%CMDCMDLINE%
 
 for /f "tokens=2" %%a in ("%CMDCMDLINE%") do (
     if "%%a"=="/c" (
+        if defined jar (
+            if defined java (
+                call :writeMessage Application %jarName% has finished.
+            )
+        )
         pause
     )
 )
